8a7b42ed7de7c20497e8ce1095743c0d4a761cd9
david.corrado <david.corrado@well.co>
2023-03-08 11:35:37 -0600

    Update

--- app/src/main/java/com/example/composebugs/MainActivity.kt
+++ app/src/main/java/com/example/composebugs/MainActivity.kt
@@ -1,11 +1,14 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.composebugs
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import com.example.composebugs.ui.theme.ComposeBugsTheme
@@ -15,11 +18,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeBugsTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Text("Android")
+                Scaffold { paddingValues ->
+                    Column(modifier = Modifier.padding(paddingValues)) {
+                        Text("Android")
+                    }
                 }
             }
         }
