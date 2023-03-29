7c5ed38de00e63a621612b8a4f4b0405d91fb8b5
david.corrado <david.corrado@well.co>
2023-03-29 13:37:26 -0500

    Compose upgrade issues

--- app/src/main/java/com/example/composebugs/MainActivity.kt
+++ app/src/main/java/com/example/composebugs/MainActivity.kt
@@ -1,16 +1,33 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package com.example.composebugs
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.composebugs.ui.theme.ComposeBugsTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,12 +35,101 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeBugsTheme {
-                Scaffold { paddingValues ->
+                Scaffold(
+                    floatingActionButton = {
+                        FloatingActionButton(onClick = { }) {
+                            Icon(imageVector = Icons.Filled.Add, contentDescription = "Add")
+                        }
+                    },
+                    topBar = {
+                        TopAppBar(
+                            title = { Text("Top App Bar") },
+                            navigationIcon = {
+                                IconButton(onClick = { }) {
+                                    Icon(
+                                        imageVector = Icons.Filled.ArrowBack,
+                                        contentDescription = "Back",
+                                        tint = Color.White
+                                    )
+                                }
+                            }
+                        )
+                    },
+                    bottomBar = {
+                        BottomBar()
+                    }
+                ) { paddingValues ->
                     Column(modifier = Modifier.padding(paddingValues)) {
                         Text("Android")
+                        BoxScreen()
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun BottomBar() {
+    val selectedIndex = remember { mutableStateOf(0) }
+    BottomNavigation {
+        BottomNavigationItem(
+            icon = {
+                Icon(imageVector = Icons.Default.Home, "")
+            },
+            label = { Text(text = "Home") },
+            selected = (selectedIndex.value == 0),
+            onClick = {
+                selectedIndex.value = 0
+            }
+        )
+
+        BottomNavigationItem(
+            icon = {
+                Icon(imageVector = Icons.Default.Favorite, "")
+            },
+            label = { Text(text = "Favorite") },
+            selected = (selectedIndex.value == 1),
+            onClick = {
+                selectedIndex.value = 1
+            }
+        )
+
+        BottomNavigationItem(
+            icon = {
+                Icon(imageVector = Icons.Default.Person, "")
+            },
+            label = { Text(text = "Profile") },
+            selected = (selectedIndex.value == 2),
+            onClick = {
+                selectedIndex.value = 2
+            }
+        )
+    }
+}
+
+@Composable
+fun BoxScreen() {
+    Row(
+        modifier = Modifier.padding(20.dp),
+        horizontalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        Column(modifier = Modifier.background(Color.LightGray).padding(10.dp).weight(1f)) {
+            Text(
+                modifier = Modifier,
+                text = "0"
+            )
+            Text(
+                text = "Active streak"
+            )
+        }
+        Column(modifier = Modifier.background(Color.LightGray).padding(10.dp).weight(1f)) {
+            Text(
+                text = "78"
+            )
+            Text(
+                text = "Total entries"
+            )
         }
     }
 }
