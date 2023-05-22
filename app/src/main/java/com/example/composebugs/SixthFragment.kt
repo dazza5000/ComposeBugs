1b5bfed99842a6c4219b77ec33a47ee33182f82c
Darran Kelinske <darran.kelinske@gmail.com>
2023-05-22 14:12:07 -0500

    reproducing focus when swipe issue

--- app/src/main/java/com/example/composebugs/SixthFragment.kt
+++ app/src/main/java/com/example/composebugs/SixthFragment.kt
@@ -4,17 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.semantics.CollectionInfo
+import androidx.compose.ui.semantics.collectionInfo
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.role
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
-import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.rememberPagerState
-import com.google.accompanist.swiperefresh.SwipeRefresh
-import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+
 
 class SixthFragment : Fragment() {
 
@@ -27,19 +41,94 @@ class SixthFragment : Fragment() {
             setContent {
                 Scaffold(
                     topBar = {
-                        TopAppBar(title = { Text("Title") })
+                        TopAppBar(
+                            title = { androidx.compose.material3.Text("Title") }
+                        )
                     }
-                ) { paddingValue ->
-                    HorizontalPager(
-                        modifier = Modifier.padding(paddingValue),
-                        count = 1,
-                        state = rememberPagerState(0)
+                ) { paddingValues ->
+                    LazyColumn(
+                        contentPadding = paddingValues,
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        state = rememberLazyListState()
                     ) {
-                        SwipeRefresh(
-                            state = rememberSwipeRefreshState(isRefreshing = false),
-                            onRefresh = { }
-                        ) {
-                            Text("Test")
+                        item {
+                            Text("foo")
+                        }
+
+                        item {
+                        Text("bar")
+
+                        }
+
+                        item {
+                            ElevatedCard(
+                                modifier = Modifier
+                                    .padding(16.dp)
+                                    .fillMaxWidth(),
+                                content = {
+
+
+                                    Row(
+                                        modifier = Modifier.semantics(mergeDescendants = true) {
+
+                                        },
+                                        horizontalArrangement = Arrangement.SpaceBetween,
+                                        verticalAlignment = Alignment.CenterVertically
+                                    ) {
+
+                                        Image(
+                                            imageVector = Icons.Filled.Create,
+                                            contentDescription = "foo",
+                                            alignment = Alignment.CenterStart,
+                                        )
+
+                                        androidx.compose.material3.Text(
+                                            "Beginning Text"
+                                        )
+
+                                        Row(
+                                            modifier = Modifier.semantics {
+                                                contentDescription = "content description"
+                                            },
+                                            horizontalArrangement = Arrangement.End
+                                        ) {
+                                            androidx.compose.material3.Text(
+                                                text = "End Text",
+                                            )
+                                        }
+                                    }
+                                    Row(
+                                        modifier = Modifier.semantics(mergeDescendants = true) {
+
+                                        },
+                                        horizontalArrangement = Arrangement.SpaceBetween,
+                                        verticalAlignment = Alignment.CenterVertically
+                                    ) {
+
+                                        Image(
+                                            imageVector = Icons.Filled.Create,
+                                            contentDescription = "foo",
+                                            alignment = Alignment.CenterStart,
+                                        )
+
+                                        androidx.compose.material3.Text(
+                                            "Beginning Text"
+                                        )
+
+                                        Row(
+                                            modifier = Modifier.semantics {
+                                                contentDescription = "content description 2"
+                                            },
+                                            horizontalArrangement = Arrangement.End
+                                        ) {
+                                            androidx.compose.material3.Text(
+                                                text = "End Text",
+                                            )
+                                        }
+                                    }
+                                }
+                            )
                         }
                     }
                 }
